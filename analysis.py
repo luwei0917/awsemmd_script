338c61e26f703978589743f8018d2dd5ac492e02
luwei0917 <luwei0917@gmail.com>
2016-06-07 11:18:32 -0500

    should use the environment now 5

--- analysis.py
+++ analysis.py
@@ -18,6 +18,8 @@ parser.add_argument("-n", "--number", type=int, default=20,
                     help="Number of simulation run")
 parser.add_argument("-m", "--movie", help="generate the movie",
                     action="store_true")
+parser.add_argument("-p", "--plotOnly", help="only generate the plot",
+                    action="store_false")
 args = parser.parse_args()
 
 list_of_max_q = []
@@ -29,54 +31,53 @@ os.system("mkdir -p results")
 for i in range(n):
     # analysis
     os.system("mkdir -p analysis/"+str(i))
-    record_time = 0
-    # move necessary file into analysis folder
     os.chdir("analysis/"+str(i))
-    sys.stdout = open("chosen.txt", "w")
-    os.system("mv ../../simulation/"+str(i)+"/dump.lammpstrj .")
-    os.system("mv ../../simulation/"+str(i)+"/wham.dat .")
-    os.system("mv ../../simulation/"+str(i)+"/energy.dat .")
-    if(args.movie):
+    if not args.plotOnly:
+        # move necessary file into analysis folder
+        sys.stdout = open("chosen.txt", "w")
+        os.system("mv ../../simulation/"+str(i)+"/dump.lammpstrj .")
+        os.system("mv ../../simulation/"+str(i)+"/wham.dat .")
+        os.system("mv ../../simulation/"+str(i)+"/energy.dat .")
+        if(args.movie):
+            os.system(
+                "python2 ~/opt/script/BuildAllAtomsFromLammps.py \
+                dump.lammpstrj movie")
+        os.system(
+            "python2 ~/opt/script/CalcRMSD.py "+protein_name+" \
+            dump.lammpstrj rmsd")
+        record_time = 0
+        with open('wham.dat') as input_data:
+            # Skips text before the beginning of the interesting block:
+            record_time = 0
+            max_q = 0
+            next(input_data)
+            for line in input_data:
+                # time, q = line.strip().split()
+
+                time = int(line.strip().split()[0])
+                q = float(line.strip().split()[1])
+                if(q > max_q):
+                    record_time = time
+                    max_q = q
+            list_of_max_q += [max_q]
+        time_step = record_time
+
+        print('ITEM: TIMESTEP')
+        with open('dump.lammpstrj') as input_data:
+            # Skips text before the beginning of the interesting block:
+            for line in input_data:
+                if line.strip() == str(time_step):
+                    print(line.strip())  # Or whatever test is needed
+                    break
+            # Reads text until the end of the block:
+            for line in input_data:  # This keeps reading the file
+                if line.strip() == 'ITEM: TIMESTEP':
+                    break
+                print(line.strip())
+        sys.stdout.close()
         os.system(
             "python2 ~/opt/script/BuildAllAtomsFromLammps.py \
-            dump.lammpstrj movie")
-    os.system(
-        "python2 ~/opt/script/CalcRMSD.py "+protein_name+" \
-        dump.lammpstrj rmsd")
-    # subprocess.Popen(
-    #     "python2 ~/opt/script/CalcRMSD.py "+protein_name+" dump.lammpstrj rmsd",
-    #     env=my_env)
-    with open('wham.dat') as input_data:
-        # Skips text before the beginning of the interesting block:
-        record_time = 0
-        max_q = 0
-        next(input_data)
-        for line in input_data:
-            # time, q = line.strip().split()
-
-            time = int(line.strip().split()[0])
-            q = float(line.strip().split()[1])
-            if(q > max_q):
-                record_time = time
-                max_q = q
-        list_of_max_q += [max_q]
-    time_step = record_time
-
-    print('ITEM: TIMESTEP')
-    with open('dump.lammpstrj') as input_data:
-        # Skips text before the beginning of the interesting block:
-        for line in input_data:
-            if line.strip() == str(time_step):
-                print(line.strip())  # Or whatever test is needed
-                break
-        # Reads text until the end of the block:
-        for line in input_data:  # This keeps reading the file
-            if line.strip() == 'ITEM: TIMESTEP':
-                break
-            print(line.strip())
-    sys.stdout.close()
-    os.system(
-        "python2 ~/opt/script/BuildAllAtomsFromLammps.py chosen.txt chosen")
+            chosen.txt chosen")
     # os.system("cp ~/opt/plot_scripts/energy.plt .")
     # os.system(  # replace PROTEIN with pdb name
     #         "sed -i.bak 's/NUMBER/'" +
@@ -97,8 +98,8 @@ for i in range(n):
             "'/g' show.tcl")
     os.chdir("../..")
     os.system("cp "+protein_name+"/*.pdb analysis/"+str(i))
-
-sys.stdout = open("analysis/list_of_max_q", "w")
-for q in list_of_max_q:
-    print(q)
-sys.stdout.close()
+if not args.plotOnly:
+    sys.stdout = open("analysis/list_of_max_q", "w")
+    for q in list_of_max_q:
+        print(q)
+    sys.stdout.close()
