55b6c58368f399b2e115aca20796bb9f7702434a
Lu <wl45@login2.biou.rice.edu>
2016-08-14 21:25:42 -0500

    minors

--- gg.py
+++ gg.py
@@ -11,39 +11,183 @@ os.environ["PATH"] = "/home/wl45/python/bin:/home/wl45/opt:" + mypath
 my_env = os.environ.copy()
 
 parser = argparse.ArgumentParser(
-        description="This is a python3 script to\
-        automatically analysis the simulation")
-
+        description="This is my playground for current project")
+parser.add_argument("protein", help="the name of protein")
 # parser.add_argument("template", help="the name of template file")
 args = parser.parse_args()
 
-result_folder = "WeiLu_Aug_07"
-os.system("mkdir -p "+result_folder)
-protein_list = ['T089', 'T120', 'T251', 'top7', '1UBQ']
-# sublist = ['_ha', '_he']
-sublist = ['_lp', '_he_lp']
-folder_list = []
-for protein in protein_list:
-    for sub in sublist:
-        folder_list += [protein+sub]
-print(folder_list)
-# exit(1)
 
-for folder in folder_list:
-    print(folder)
-    os.chdir(folder)
-    exec (open("config.py").read())
-    n = number_of_run
-    steps = simulation_steps
-    os.system("mkdir -p ../{}/".format(result_folder)+folder+"/best_q")
-    os.system("sort analysis/list_of_max_q > ../{}/q_".format(result_folder)+folder+".dat")
-    for i in range(n):
-        # move
-        os.chdir("analysis/"+str(i))
-        os.system("cp chosen.pdb ../../../{}/".format(result_folder) + folder+"/best_q/"+str(i)+".pdb")
-        os.chdir("../..")
-    os.chdir("..")
+w_helix_list = [0.1, 0.5, 1, 1.5]
+m_helix_list = [0.1, 0.5, 1, 1.5]
+for i in range(len(w_helix_list)):
+    w = w_helix_list[i]
+    for j in range(len(m_helix_list)):
+        m = m_helix_list[j]
+        folder_name = str(i)+"_"+str(j)
+        os.system("mkdir "+folder_name)
+        os.chdir(folder_name)
+        os.system("cp -r ../2xov .")
+        os.chdir("2xov")
+        os.system(
+                "sed -i.bak 's/W_HELIX/'" +
+                str(w) +
+                "'/g' fix_backbone_coeff.data")
+        os.system(
+                "sed -i.bak 's/M_HELIX/'" +
+                str(m) +
+                "'/g' fix_backbone_coeff.data")
+        os.chdir("..")
+        os.system("run.py 2xov/ -n 5")
+
+os.system("cp ~/opt/gg.py this_gg.py")
+# for i in range(5):
+#     os.system("mkdir "+str(i))
+#     os.chdir(str(i))
+#     os.system("cp -r ../2xov/ .")
+#     os.system("cp ../../2xov_strong_single_memory_600to500/simulation/"+str(i)+"/restart.2000000 2xov/")
+#     os.system("run.py -s 4 -n 2 2xov/")
+#     os.chdir("..")
+
+# # rama_list = [6, 8, 16]
+# # rama_list = [4]
+# melt_t_list = [400, 500, 600]
+# for variable in melt_t_list:
+#     folder_name = str(variable)
+#     os.system("mkdir "+folder_name)
+#     os.chdir(folder_name)
+#     os.system("cp -r ../1qjp .")
+#     os.chdir("1qjp")
+#     os.system(
+#             "sed -i.bak 's/MELTT/'" +
+#             str(variable) +
+#             "'/g' 1qjp.in")
+#     os.chdir("..")
+#     # os.system("pwd")
+#     os.system("run.py 1qjp/ -n 5 -s 5")
+#     os.chdir("..")
+# os.system("cp ~/opt/gg.py this_gg.py")
+#
+# exec (open("config.py").read())
+# n = number_of_run
+# steps = simulation_steps
+#
+# protein_name = args.protein.strip('/')
+#
+# temp = 400
+# folder_name = "{}_t{}_q100_test11".format(protein_name, str(temp))
+# print("all going to "+folder_name)
+# os.system("mkdir -p "+folder_name)
+# os.system("rm -f "+folder_name + "/*")
+# command = 'cat simulation/{}/%d/wham11 \
+# >> {}/all_wham.dat'.format(temp, folder_name)
+# # cal rmsd
+# os.chdir("simulation/"+str(temp))
+# for i in range(n):
+#     os.chdir(str(i))
+#     os.system("awk '{print>\"file1\"(NR>(n/2)?2:1)}' n=\"$(wc -l <file1)\" file1")
+#     os.system("cat file11 >> ../../../"+folder_name+"/rmsd_total")
+#     # os.system("sed 1d wham.dat > wham1d.dat")
+#     os.system("awk '{print>\"wham1\"(NR>(n/2)?2:1)}' n=\"$(wc -l <wham1)\" wham1")
+#     os.chdir("..")
+# os.chdir("../..")
+# for i in range(n):
+#     cmd = command % i
+#     os.system(cmd)
+# os.chdir(folder_name)
+# os.system("awk '{print $2}' all_wham.dat > Qw_total")
+# os.system("awk '{print $3}' all_wham.dat > rg_total")
+# os.system("awk '{print $4}' all_wham.dat > p_total")
+# os.system("awk '{print $5}' all_wham.dat > tc_total")
+# os.system("awk '{print $NF}' all_wham.dat > e_total")
+# os.system("cp ~/opt/wham_analysis/*.m .")
+# os.chdir("..")
+# os.system("~/opt/script/wham/fused_calc_cv.sc {} top7 50 400 350 450 5 50 100 0 0.98".format(folder_name))
+#
+#
+# folder_name = "{}_t{}_q100_test12".format(protein_name, str(temp))
+# print("all going to "+folder_name)
+# os.system("mkdir -p "+folder_name)
+# os.system("rm -f "+folder_name + "/*")
+# command = 'cat simulation/{}/%d/wham12 \
+# >> {}/all_wham.dat'.format(temp, folder_name)
+# # cal rmsd
+# os.chdir("simulation/"+str(temp))
+# for i in range(n):
+#     os.chdir(str(i))
+#     os.system("cat file12 >> ../../../"+folder_name+"/rmsd_total")
+#     os.chdir("..")
+# os.chdir("../..")
+# for i in range(n):
+#     cmd = command % i
+#     os.system(cmd)
+# os.chdir(folder_name)
+# os.system("awk '{print $2}' all_wham.dat > Qw_total")
+# os.system("awk '{print $3}' all_wham.dat > rg_total")
+# os.system("awk '{print $4}' all_wham.dat > p_total")
+# os.system("awk '{print $5}' all_wham.dat > tc_total")
+# os.system("awk '{print $NF}' all_wham.dat > e_total")
+# os.system("cp ~/opt/wham_analysis/*.m .")
+# os.chdir("..")
+#
+#
+#
+# os.system("~/opt/script/wham/fused_calc_cv.sc {} top7 50 400 350 450 5 50 100 0 0.98".format(folder_name))
+
+#
+# result_folder = "WeiLu_Aug_07"
+# os.system("mkdir -p "+result_folder)
+# protein_list = ['T089', 'T120', 'T251', 'top7', '1UBQ']
+# # sublist = ['_ha', '_he']
+# sublist = ['_lp', '_he_lp']
+# folder_list = []
+# for protein in protein_list:
+#     for sub in sublist:
+#         folder_list += [protein+sub]
+# print(folder_list)
+# # exit(1)
+# # awk '{print>'file'(NR>(n/2)?2:1)}' n='$(wc -l <test)' test
+# for folder in folder_list:
+#     print(folder)
+#     os.chdir(folder)
+#     exec (open("config.py").read())
+#     n = number_of_run
+#     steps = simulation_steps
+#     os.system("mkdir -p ../{}/".format(result_folder)+folder+"/best_q")
+#     os.system("sort analysis/list_of_max_q > ../{}/q_".format(result_folder)+folder+".dat")
+#     for i in range(n):
+#         # move
+#         os.chdir("analysis/"+str(i))
+#         os.system("cp chosen.pdb ../../../{}/".format(result_folder) + folder+"/best_q/"+str(i)+".pdb")
+#         os.chdir("../..")
+#     os.chdir("..")
 # Useful codes
 # os.system("awk '{print $NF}' all_wham.dat > e_total")
 # tr " " "\n"
 # sed 1d
+
+# result_folder = "WeiLu_Aug_07"
+# os.system("mkdir -p "+result_folder)
+# protein_list = ['T089', 'T120', 'T251', 'top7', '1UBQ']
+# # sublist = ['_ha', '_he']
+# sublist = ['_lp', '_he_lp']
+# folder_list = []
+# for protein in protein_list:
+#     for sub in sublist:
+#         folder_list += [protein+sub]
+# print(folder_list)
+# # exit(1)
+#
+# for folder in folder_list:
+#     print(folder)
+#     os.chdir(folder)
+#     exec (open("config.py").read())
+#     n = number_of_run
+#     steps = simulation_steps
+#     os.system("mkdir -p ../{}/".format(result_folder)+folder+"/best_q")
+#     os.system("sort analysis/list_of_max_q > ../{}/q_".format(result_folder)+folder+".dat")
+#     for i in range(n):
+#         # move
+#         os.chdir("analysis/"+str(i))
+#         os.system("cp chosen.pdb ../../../{}/".format(result_folder) + folder+"/best_q/"+str(i)+".pdb")
+#         os.chdir("../..")
+#     os.chdir("..")
--- movie.py
+++ movie.py
@@ -9,12 +9,14 @@ parser = argparse.ArgumentParser(
         description="written by Wei Lu."
 )
 parser.add_argument("protein", help="The name of the protein")
-
+parser.add_argument("-p", "--plot", help="only plot mode",
+                    action="store_true")
 args = parser.parse_args()
 protein_name = args.protein.split('.')[0]
-os.system(
-    "python2 ~/opt/script/BuildAllAtomsFromLammps.py \
-    dump.lammpstrj movie")
+if(not args.plot):
+    os.system(
+        "python2 ~/opt/script/BuildAllAtomsFromLammps.py \
+        dump.lammpstrj movie")
 os.system("cp ~/opt/plot_scripts/*.tcl .")
 os.system(  # replace PROTEIN with pdb name
         "sed -i.bak 's/PROTEIN/'" +
