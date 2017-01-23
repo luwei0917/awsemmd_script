1452c9d83528b8f3d8fdc3b04e1999e8c5ce2031
luwei0917 <luwei0917@gmail.com>
2017-01-23 16:25:11 -0600

    updates

--- frustratometer_run.py
+++ frustratometer_run.py
@@ -10,10 +10,20 @@ from datetime import datetime
 import imp
 from myPersonalFunctions import *
 import glob
-
+# Useful codes
+# os.system("awk '{print $NF}' all_wham.dat > e_total")
+# tr " " "\n"
+# sed 1d
+# sort -u -k 3
+# sed -e 's/+T//'
+# awk '$5=-$5' data
+mypath = os.environ["PATH"]
+os.environ["PATH"] = "/home/wl45/python/bin:/home/wl45/opt:" + mypath
+my_env = os.environ.copy()
 
 parser = argparse.ArgumentParser(description="This is my playground for current project")
 # parser.add_argument("protein", help="the name of protein")
+# parser.add_argument("template", help="the name of template file")
 parser.add_argument("-t", "--test", help="test ", action="store_true", default=False)
 parser.add_argument("-n", "--number", type=int, default=10, help="number of run")
 parser.add_argument("-d", "--debug", action="store_true", default=False)
@@ -31,7 +41,7 @@ if(args.test):
     structures = ["ga", "gb"]
     for structure in structures:
         sequence_list = ["ga77", "gb77"]
-        # sequence_list = ["gb77", "gb88b", "gb91", "gb95", "gb", "ga", "ga95", "ga91", "ga88", "ga77"]
+        sequence_list = ["gb77", "gb88b", "gb91", "gb95", "gb", "ga", "ga95", "ga91", "ga88", "ga77"]
         for sequence in sequence_list:
             folder = structure+"_" + sequence
             do("cp -r gagb "+folder)
@@ -43,7 +53,4 @@ if(args.test):
                 do("~/bin/lmp_serial_old < {}.in".format(mode))
                 do("mv tertiary_frustration.dat tertiary_frustration_{}.dat".format(mode))
                 do("mv tertiary_frustration.tcl tertiary_frustration_{}.tcl".format(mode))
-                do("awk '{ total += $19 } END { print total/NR }' tertiary_frustration_configuration.dat > average_f")
-                do("echo '{}' >> ../average_f".format(sequence))
-                do("cat average_f >> ../average_f")
             cd("..")
--- pulling_plot.py
+++ pulling_plot.py
@@ -36,11 +36,14 @@ parser.add_argument("-n", "--number", type=int, default=10,
                     help="number")
 parser.add_argument("--minor", type=int, default=1,
                     help="minor control")
+parser.add_argument("--one", action="store_true", default=False)
+parser.add_argument("--multi", action="store_true", default=False)
 parser.add_argument("-s", "--save", action="store_true", default=False)
 parser.add_argument("-r", "--reproduce", default=None)
 parser.add_argument("-t", "--test", action="store_true", default=False)
 parser.add_argument("-m", "--mode", default="pulling")
 parser.add_argument("-d", "--debug", action="store_true", default=False)
+
 args = parser.parse_args()
 
 
@@ -71,29 +74,6 @@ if(args.test):
         do("pulling_plotcontour.py")
         do("cp test.png ../results/{}.png".format(force))
         cd(cwd)
-    # output = args.outname
-    # temp = args.temperature
-    # ax = plt.subplot(1, 1, 1)
-    # force_list = np.linspace(0.5,3,26)
-    # i = 1
-    # for force in force_list[10:16:1]:
-    #     folder = "wham_" + str(i) + "_force_" + str(force)
-    #     name = folder + '/pmf-'+str(temp)+'.dat'
-    #     data = pd.read_table(name, sep='\s+', comment='#', names=["bin","bin_center_1","f","df","e","s"])
-    #     # print(data)
-    #     # data.plot(ax=ax, x='bin_center_1', y='f', linewidth=5.0)
-    #     data.plot(ax=ax, x='bin_center_1', y='f',xlim=(0, 150), label="F= \n"+str(force))
-    # ax.set_xlabel("Distance(Å)")
-    # ax.set_ylabel("free energy(kT)")
-    # # ax.set_title("Force at 0.7 Kcal/mole-Angstrom")
-    # # ax.legend.remove()
-    # ax.legend(loc='center left', bbox_to_anchor=(1, 0.6))
-    # plt.gcf().subplots_adjust(bottom=0.15)
-    # plt.gcf().subplots_adjust(left=0.15)
-    # plt.gcf().subplots_adjust(right=0.80)
-    # fig = plt.gcf()
-    # fig.savefig(output)
-    # os.system("open " + output)
 
 if(args.save):
     # print(os.getcwd())
@@ -104,6 +84,56 @@ if(args.save):
         pickle.dump(args, f)
     os.system("cp ~/opt/plot.py plot_{}.py".format(datetime.datetime.now().strftime("%Y%m%d-%H%M")))
 
+if(args.one):
+    output = args.outname
+    temp = args.temperature
+    ax = plt.subplot(1, 1, 1)
+
+    name = 'pmf-'+str(temp)+'.dat'
+    data = pd.read_table(name, sep='\s+', comment='#', names=["bin","bin_center_1","f","df","e","s"])
+    # print(data)
+    # data.plot(ax=ax, x='bin_center_1', y='f', linewidth=5.0)
+    data.plot(ax=ax, x='bin_center_1', y='f',xlim=(0, 200), label="T= \n"+str(temp))
+    ax.set_xlabel("Distance(Å)")
+    ax.set_ylabel("free energy(kT)")
+    # ax.set_title("Force at 0.7 Kcal/mole-Angstrom")
+    ax.legend_.remove()
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.6))
+    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.gcf().subplots_adjust(left=0.15)
+    plt.gcf().subplots_adjust(right=0.80)
+    fig = plt.gcf()
+    fig.savefig(output)
+    os.system("open " + output)
+
+if(args.multi):
+    output = args.outname
+    temp = args.temperature
+    ax = plt.subplot(1, 1, 1)
+
+    sequence = "a206g"
+    force = 1.0
+    i = 1
+    force_list = [1.6, 1.8, 2.0]
+    for force in force_list:
+        target = str(i)+"_force_"+str(force)+"_"+sequence+"/"
+        # target = "1_force_1.0_a206g/"
+        name = target + 'pmf-'+str(temp)+'.dat'
+        data = pd.read_table(name, sep='\s+', comment='#', names=["bin","bin_center_1","f","df","e","s"])
+        # print(data)
+        # data.plot(ax=ax, x='bin_center_1', y='f', linewidth=5.0)
+        data.plot(ax=ax, x='bin_center_1', y='f',xlim=(0, 200), label=str(force))
+    ax.set_xlabel("Distance(Å)")
+    ax.set_ylabel("free energy(kT)")
+    # ax.set_title("Force at 0.7 Kcal/mole-Angstrom")
+    ax.legend_.remove()
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.6))
+    plt.gcf().subplots_adjust(bottom=0.15)
+    plt.gcf().subplots_adjust(left=0.15)
+    plt.gcf().subplots_adjust(right=0.80)
+    fig = plt.gcf()
+    fig.savefig(output)
+    os.system("open " + output)
 
 if(args.all_temp > 0):
     print("Hello World GaGb1")
@@ -142,238 +172,3 @@ if(args.all_temp > 0):
     fig = plt.gcf()
     fig.savefig(output)
     os.system("open " + output)
-
-
-def gagb_compare():
-    print("Hello World gagb_compare")
-    output = args.outname
-    temp = args.temperature
-    ax = plt.subplot(1, 1, 1)
-    # ax = plt.figure()
-    ax.set_title("gagb")
-
-    # name = 'pmf-'+str(temp)+'.dat'
-    target_list = ["gb77", "gb88b", "gb91", "gb95", "gb", "ga", "ga95", "ga88", "ga77"]
-    # target_list = ["gb77", "gb88b", "gb91", "gb95", "gb"]
-    for target in target_list:
-        name = target+"-"+str(temp)+'.dat'
-        data = pd.read_table(name, sep='\s+', comment='#', names=["bin","bin_center_1","f","df","e","s"])
-        print(data["f"].iloc[-1])
-        data["f"] = data["f"] - data["f"].iloc[-8]
-        if(target == "ga95"):
-            print(data["f"])
-        data.plot(ax=ax, x='bin_center_1', y='f', label=target, linewidth=3.0)
-    # name = 'gb-'+str(temp)+'.dat'
-    # data2 = pd.read_table(name, sep='\s+', comment='#', names=["bin","bin_center_1","f","df","e","s"])
-    # data2.plot(ax=ax, x='bin_center_1', y='f', label="gb")
-    plt.ylabel("free energy is units of kT")
-    plt.xlabel("qo based on ga's sturcture")
-
-    # ax.set_legend_bgcolor('white')
-
-    ax.legend(loc='center left', bbox_to_anchor=(1, 0.8), frameon=1)
-    # ax.set_axis_bgcolor('white')
-    # ax.legend.set_facecolor('white')
-    # legend = plt.legend(frameon = 1)
-    # frame = legend.get_frame()
-    # frame.set_facecolor('green')
-    plt.gcf().subplots_adjust(bottom=0.15)
-    plt.gcf().subplots_adjust(left=0.15)
-    plt.gcf().subplots_adjust(right=0.80)
-    fig = plt.gcf()
-    fig.savefig(output, transparent=True)
-    os.system("open " + output)
-    # data.show()
-if(args.gagb_compare):
-    gagb_compare()
-
-
-def gagb():
-    print("Hello World GaGb")
-    output = args.outname
-    temp = args.temperature
-
-    name = 'pmf-'+str(temp)+'.dat'
-    data = pd.read_table(name, sep='\s+', comment='#', names=["bin","bin_center_1","f","df","e","s"])
-    print(data)
-    data.plot(x='bin_center_1', y='f')
-    fig = plt.gcf()
-    fig.savefig(output)
-    os.system("open " + output)
-    # data.show()
-if(args.gagb):
-    gagb()
-# def qnqc():
-#     print("Hello World")
-#     n = 2
-#     f, axarr = plt.subplots(4, 4, sharex='col', sharey='row')
-
-#     for i in range(n):
-#         name = str(i) + "/qnqc"
-#         data = pd.read_table(name, header=None, names=["qn", "qc"])
-
-#         plt.tick_params(
-#             axis='x',
-#             which='both',
-#             bottom='off',
-#             top='off',
-#             labelbottom='off'
-#         )
-#         data.plot(ax=axarr[i, i], legend=False)
-#     # print(data)
-#     # data.plot()
-#     # plt.plot(data[1])
-#     # plt.show()
-#     # ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-#     # df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
-#     # df = df.cumsum()
-#     # fig = plt.figure()
-
-#     # plt.subplot(4, 4, 13)
-#     # plt.plot(range(12))
-#     fig = plt.gcf()
-#     fig.savefig('figure.pdf')
-#     os.system("open figure.pdf")
-#     # data.show()
-
-
-def qnqc():
-    print("Hello World")
-    n = args.number
-    for i in range(n):
-        name = str(i) + "/qnqc"
-        data = pd.read_table(name, header=None, names=["qn", "qc"])
-        ax = plt.subplot(4, 5, i + 1)
-        plt.tick_params(
-            axis='x',
-            which='both',
-            bottom='off',
-            top='off',
-            labelbottom='off'
-        )
-        plt.tick_params(
-            axis='y',
-            which='both',
-            right='off',
-        )
-        data.plot(ax=ax, legend=False, ylim=(0, 1))
-    # print(data)
-    # data.plot()
-    # plt.plot(data[1])
-    # plt.show()
-    # ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-    # df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
-    # df = df.cumsum()
-    # fig = plt.figure()
-    # plt.subplot(4, 4, 13)
-    # plt.plot(range(12))
-    plt.suptitle('Red is Qn, Blue is Qc')
-    fig = plt.gcf()
-    output = args.outname
-    fig.savefig(output)
-    os.system("open " + output)
-    # fig.savefig('figure.pdf')
-    # os.system("open figure.pdf")
-    # data.show()
-if(args.qnqc):
-    qnqc()
-
-
-def qnqc_pull():
-    print("Hello World")
-    n = args.number
-    for i in range(n):
-        if(args.minor == 1):
-            name = "" + str(i) + "/addforce.dat"
-        elif(args.minor == 2):
-            name = "rerun_" + str(i) + "/addforce.dat"
-        elif(args.minor == 3):
-            name = "restart_" + str(i) + "/addforce.dat"
-            name2 = "rerun_" + str(i) + "/addforce.dat"
-            data2 = pd.read_table(name2, sep='\s+')
-        data = pd.read_table(name, sep='\s+')
-        # print(data)
-        # data = pd.read_table(name, header=None, names=["qn", "qc"])
-        ax = plt.subplot(4, 5, i + 1)
-        plt.tick_params(
-            axis='x',
-            which='both',
-            bottom='off',
-            top='off',
-            labelbottom='off'
-        )
-        plt.tick_params(
-            axis='y',
-            which='both',
-            right='off',
-        )
-        if(args.minor == 1):
-            data["addedFroce"] = -data["addedFroce"]
-            data.plot(ax=ax, legend=False, x="position", y="addedFroce", xlim=(0, 400), ylim=(0, 2))
-        elif(args.minor == 2):
-            data["addedFroce"] = data["addedFroce"]/2.0
-            data.plot(ax=ax, legend=False, x="Distance", y="addedFroce", xlim=(0, 400), ylim=(0, 2))
-        elif(args.minor == 3):
-            data["addedFroce"] = -data["addedFroce"]
-            data.plot(ax=ax, legend=False, x="Distance", y="addedFroce", xlim=(0, 600), ylim=(0, 2))
-            # data2["addedFroce"] = -data2["addedFroce"]
-            data2.plot(ax=ax, legend=False, x="Distance", y="addedFroce", xlim=(0, 600), ylim=(0, 2))
-
-    # print(data)
-    # data.plot()
-    # plt.plot(data[1])
-    # plt.show()
-    # ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
-    # df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
-    # df = df.cumsum()
-    # fig = plt.figure()
-    # plt.subplot(4, 4, 13)
-    # plt.plot(range(12))
-    # plt.suptitle('Red is Qn, Blue is Qc')
-    fig = plt.gcf()
-    output = args.outname
-    fig.savefig(output)
-    os.system("open " + output)
-    # fig.savefig('figure.pdf')
-    # os.system("open figure.pdf")
-    # data.show()
-if(args.qnqc_pull):
-    qnqc_pull()
-# exec(open("config.py").read())
-# # print(n, x, y, type(y))
-# n = number_of_run
-# steps = simulation_steps
-# # protein_name
-#
-# # print(n, steps)
-# # sys.exit(0)
-#
-# os.system("mkdir -p results")
-# os.system("cp ~/opt/plot_scripts/qw_all.plt .")
-# os.system("gnuplot -e 'number_of_run={}' qw_all.plt".format(n-1))
-# for i in range(n):
-#     print(i)
-#     # analysis
-#     os.chdir("analysis/"+str(i))
-#     os.system("cp ~/opt/plot_scripts/*.plt .")
-#     os.system("gnuplot qw.plt")
-#     os.system("mv qw.pdf ../../results/qw_{0}.pdf".format(str(i)))
-#     os.chdir("../..")
-# protein_name = args.template.strip('/')
-# os.system("cp ~/opt/plot_scripts/free_energy.plt .")
-# os.system("gnuplot free_energy.plt ")
-# os.system("open free_energy.pdf")
-#
-# print("Hello World GaGb")
-# output = args.outname
-# temp = args.temperature
-# ax = plt.subplot(1, 1, 1)
-#
-# name = 'pmf-'+str(temp)+'.dat'
-# data = pd.read_table(name, sep='\s+', comment='#', names=["bin","bin_center_1","f","df","e","s"])
-# print(data)
-# data.plot(ax=ax, x='bin_center_1', y='f', label=str(temp))
-# ax.set_xlabel("Q of gb")
-# fig = plt.gcf()
-# fig.savefig(output)
-# os.system("open " + output)
